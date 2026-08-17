49ce89271f98baf3f8d0ff09159031adb40d1467
Seodam <qkrclsp1004@gmail.com>
2026-08-17 16:51:31 +0900

    Fix tests for authentication

--- test/controllers/words_controller_test.rb
+++ test/controllers/words_controller_test.rb
@@ -1,18 +1,52 @@
 require "test_helper"
 
 class WordsControllerTest < ActionDispatch::IntegrationTest
+  def login_as(user)
+    post login_path, params: {
+      email: user.email,
+      password: "password"
+    }
+  end
+
   test "should get index" do
+    user = User.create!(
+      name: "Test User",
+      email: "index@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+
+    login_as(user)
+
     get words_path
+
     assert_response :success
   end
 
   test "should get new" do
+    user = User.create!(
+      name: "Test User",
+      email: "new@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+
+    login_as(user)
+
     get "/words/new"
+
     assert_response :success
   end
 
   test "should create word" do
-    user = User.first
+    user = User.create!(
+      name: "Test User",
+      email: "create@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+
+    login_as(user)
 
     assert_difference("Word.count", 1) do
       post words_path, params: {
@@ -25,8 +59,17 @@ class WordsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should edit word" do
+    user = User.create!(
+      name: "Test User",
+      email: "edit@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+
+    login_as(user)
+
     word = Word.create!(
-      user: User.first,
+      user: user,
       word: "orange",
       meaning: "オレンジ"
     )
@@ -37,24 +80,44 @@ class WordsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update word" do
+    user = User.create!(
+      name: "Test User",
+      email: "update@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+
+    login_as(user)
+
     word = Word.create!(
-      user: User.first,
+      user: user,
       word: "grape",
       meaning: "ぶどう"
     )
 
     patch "/words/#{word.id}", params: {
-      word: "grape",
-      meaning: "ブドウ"
+      word: {
+        word: "grape",
+        meaning: "ブドウ"
+      }
     }
 
-    assert_redirected_to words_path
+    assert_redirected_to word_path(word)
     assert_equal "ブドウ", word.reload.meaning
   end
 
   test "should destroy word" do
+    user = User.create!(
+      name: "Test User",
+      email: "destroy@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+
+    login_as(user)
+
     word = Word.create!(
-      user: User.first,
+      user: user,
       word: "melon",
       meaning: "メロン"
     )
